f2bfc9adfedc765d56ee92d5a4319b4976ab555e
matheus695p <mateu_695p@hotmail.com>
2021-02-07 11:56:56 -0300

    rename: empezando a renombrar

--- src/basic/face_detection.py
+++ src/basic/face_detection.py
@@ -10,6 +10,8 @@ img = cv2.imread(path)
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+faces = faceCascade.detectMultiScale(imgGray)
+
 
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
